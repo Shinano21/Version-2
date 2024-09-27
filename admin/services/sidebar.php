5ce78d86e66fcfcebbea6d1f81b05e83f27fbe39
Jan-Jan20 <janjanarimado.alberto@bicol-u.edu.ph>
2024-09-27 18:00:41 +0800

    sidebar.php:logo change

--- admin/services/sidebar.php
+++ admin/services/sidebar.php
@@ -3,7 +3,7 @@
         <div class="nano-content" style="text-align:justify;text-indent:15%;">
             <ul>
                 <div class="logo"><a href="home.php">
-                        <img src="../src/logo.png" style="width:80%;margin-left:-50%;" alt="" /> </a>
+                        <img src="../src/logo.svg" style="width:80%;margin-left:-50%;" alt="" /> </a>
                 </div>
                 <li><a href="../home.php"><i class="fa fa-home"></i> Dashboard </a></li>
                 <li><a href="../residents.php"><i class="fa fa-users"></i> Residents</a></li>
