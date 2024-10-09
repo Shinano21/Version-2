03514cf4c560c46d5a06556492692598e3c0d515
Jan-Jan20 <janjanarimado.alberto@bicol-u.edu.ph>
2024-10-09 15:16:19 +0800

    wsContactUsSettings.php:fix errors

--- admin/wsContactUsSettings.php
+++ admin/wsContactUsSettings.php
@@ -1,15 +1,36 @@
 <?php
 session_start();
-
 include "dbcon.php";
+
 if (!isset($_SESSION["user"]) || $_SESSION["user_type"] == "System Administrator") {
     header("Location: index.php");
     exit();
 }
+
+// Initialize variables with default values
+$shortMess = '';
+$email = '';
+$contact = '';
+$address = '';
+$fbAcc = '';
+$fbLink = '';
+
+// Fetch data from the database if necessary
+// Example query: 
+// $query = "SELECT * FROM contact_us_settings LIMIT 1";
+// $result = mysqli_query($conn, $query);
+// if ($row = mysqli_fetch_assoc($result)) {
+//     $shortMess = $row['short_mess'];
+//     $email = $row['email'];
+//     $contact = $row['contact'];
+//     $address = $row['address'];
+//     $fbAcc = $row['fb_acc'];
+//     $fbLink = $row['fb_link'];
+// }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <title>Contact Us Settings | CareVisio</title>
     <?php include "../user/data/contact_us.php"; ?>
@@ -17,7 +38,6 @@ if (!isset($_SESSION["user"]) || $_SESSION["user_type"] == "System Administrator
     <link rel="stylesheet" href="css/wsHome.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-
 <body onload="display_ct();">
 
     <?php include "partials/sidebar.php"?>
@@ -36,33 +56,27 @@ if (!isset($_SESSION["user"]) || $_SESSION["user_type"] == "System Administrator
                                 <h5>Contact Us</h5>
                                 <div class="formInput" style="width: 100%;">
                                     <label>Short Message</label>
-                                    <textarea name="short_mess" placeholder="Enter data"
-                                        required><?php echo $shortMess ?></textarea>
+                                    <textarea name="short_mess" placeholder="Enter data" required><?php echo $shortMess ?></textarea>
                                 </div>
                                 <div class="formInput">
                                     <label>Email</label>
-                                    <input type="text" value="<?php echo $email ?>" name="email"
-                                        placeholder="Enter data" required>
+                                    <input type="text" value="<?php echo $email ?>" name="email" placeholder="Enter data" required>
                                 </div>
                                 <div class="formInput">
                                     <label>Contact Number</label>
-                                    <input type="text" value="<?php echo $contact ?>" name="contact"
-                                        placeholder="Enter data" required>
+                                    <input type="text" value="<?php echo $contact ?>" name="contact" placeholder="Enter data" required>
                                 </div>
                                 <div class="formInput" style="width: 100%;">
                                     <label>Address</label>
-                                    <input type="text" value="<?php echo $address ?>" name="address"
-                                        placeholder="Enter data" required>
+                                    <input type="text" value="<?php echo $address ?>" name="address" placeholder="Enter data" required>
                                 </div>
                                 <div class="formInput" style="width: 100%;">
                                     <label>Facebook Account Name</label>
-                                    <input type="text" value="<?php echo $fbAcc ?>" name="fb_acc"
-                                        placeholder="Enter data" required>
+                                    <input type="text" value="<?php echo $fbAcc ?>" name="fb_acc" placeholder="Enter data" required>
                                 </div>
                                 <div class="formInput" style="width: 100%;">
                                     <label>Facebook Account Link</label>
-                                    <input type="text" value="<?php echo $fbLink ?>" name="fb_link"
-                                        placeholder="Enter data (put 'https://' at the start)" required>
+                                    <input type="text" value="<?php echo $fbLink ?>" name="fb_link" placeholder="Enter data (put 'https://' at the start)" required>
                                 </div>
                                 <div style="width: 100%; display: flex; justify-content: end; align-items: end;">
                                     <button type="submit" name="submit">Save</button>
@@ -72,11 +86,11 @@ if (!isset($_SESSION["user"]) || $_SESSION["user_type"] == "System Administrator
 
                     </div>
 
-
                 </section>
             </div>
         </div>
     </div>
+
     <script>
         function display_c() {
             var refresh = 1000; // Refresh rate in milliseconds
