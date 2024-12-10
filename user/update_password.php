c35277db6387356b5add0b4dc77139ec67318c88
Shinano29 <112055468+Shinano21@users.noreply.github.com>
2024-12-10 16:52:20 +0800

    database connection fixed

--- user/update_password.php
+++ user/update_password.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "ias2";
+    $dbname = "carevisio";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
