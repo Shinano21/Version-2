b239d74bec34b547a9b0ca1289dfdb7d58612963
Shinano29 <112055468+Shinano21@users.noreply.github.com>
2025-01-09 17:45:53 +0800

    added some changes

--- admin/wsContactUsSettings.php
+++ admin/wsContactUsSettings.php
@@ -70,8 +70,9 @@ if ($row = mysqli_fetch_assoc($result)) {
     <?php include "../user/data/contact_us.php"; ?>
     <?php include "partials/head.php"; ?>
     <link rel="stylesheet" href="css/wsHome.css">
-    <link href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" rel="stylesheet" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="..leaflet/leaflet.css" />
+<script src="..leaflet//leaflet.js"></script>
+
     <style>
           body{
     background-color: #CDE8E5;
